cb71a84a0feadd8ea66d8c6d60f46eefc480eddb
PimvBuren <6043915@mborijnland.nl>
2025-01-14 15:19:59 +0100

    pseu-code
    update pseu code

--- php/QUIZ.php
+++ php/QUIZ.php
@@ -13,13 +13,31 @@ try {
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-// lees inloggegens en kijk of ze overeen komen 
-// kijk of de vraag klopt
-// geef op het scherm weer of de vraag klopt
-// ga naar de volgende vraag
-// geef punten voor goede antwoord
-// zet ze op volgworden van meeste punten
-// geef de 3 hoogste een plek
+// wachtwoord aanmaken en mail krijgen
 
+// select data in data base
+    // vragen uit de datatbase halen
+    // vragen maken
+
+    // lees inloggegens en kijk of ze overeen komen 
+  //als het wel zo is komt er te staan ingelod
+  // als het niet goed is komt er te staan niet geldig wachtwoord of username 
+
+// als je bent ingelogd als admin ga naar admin.php
+
+
+  // kijk of het antwoord goed is 
+    // als het goed is ga naar de volgende vraag 
+    // laat het op het scherm zien dat het goed is
+      // geef punten als het goed is
+        // tel de punten bij elkaar op
+        // toon hoeveel punten je hebt
+        // ga dan naar de volgende vraag
+    // als het fout is komt er te staan fout en ga dan door naar volgende vraag
+    // laat het op het scherm zien dat het fout is
+      // en geef geen punten
+      // tel de punten bij elkaar op
+        // toon hoeveel punten je hebt
+          // ga dan naar de volgende vraag
 ?>
 
---
+++ php/admin.php
@@ -0,0 +1,16 @@
+<!-- 
+ //als je bent ingelogd als admin
+        //kan je vragen maken zodat andere mensen die kunnen zien
+        //kan je vragen verwijderen
+
+//kan je telkens zien wie de meeste punten heeft
+
+//timer maken voor elke vraag
+    // een timer instellen per vraag
+    // zodat iedereen de timer kan zien
+        //als de timer voorbij is komt er staan niks ingvuld
+        //dan krijg je geen punten
+
+
+
+//uitloggen ga naar QUIZ.php
